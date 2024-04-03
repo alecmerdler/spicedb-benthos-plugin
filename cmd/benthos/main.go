88775e44f8612db533a4904397f1a4c0172abac8
Alec Merdler <alecmerdler@gmail.com>
2024-04-02 21:35:54 -0400

    Initial plugin implementation
    Basic functioning Benthos plugin which enables using the
    ReadRelationships API from SpiceDB as an input component to
    a Benthos pipeline.

--- cmd/benthos/main.go
+++ cmd/benthos/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+
 	_ "github.com/benthosdev/benthos/v4/public/components/all"
-	"github.com/benthosdev/benthos/v4/public/service"
+
 	_ "github.com/alecmerdler/spicedb-benthos-plugin/v2/plugin"
+	"github.com/benthosdev/benthos/v4/public/service"
 )
 
 func main() {
--- cmd/serverless/benthos-lambda/main.go
+++ cmd/serverless/benthos-lambda/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"context"
+
 	_ "github.com/benthosdev/benthos/v4/public/components/all"
 	"github.com/benthosdev/benthos/v4/public/components/aws"
+
 	_ "github.com/alecmerdler/spicedb-benthos-plugin/v2/plugin"
 )
 
